f0068557ba27efe63c11f0e3457c88cb2bbdfb13
Larry Arrington <larryarrington@pursuit.org>
2023-04-27 16:41:47 -0400

    Starting over

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from "react";
-
-import birdData from "./data/birds";
 import { useState } from "react";
-import PageForCards from ".Components/PageForBirdCards";
+import birdData from "./data/birds";
+import Bird from "./components/Bird";
+import "./App.css";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 
 function App() {
+  const [cart, setCart] = useState([]);
+  const addToCart = (bird) => {
+    bird = { ...bird, id: Math.random() };
+    setCart((prev) => [...prev, bird]);
+  };
+
   return (
-    <div>
-      <Cart cart={cart} setcart={setcart} />
-      <PageForBirdCards birds={birdData} setCart={setCart} cart={cart} />
+    <div className="app">
+      <h1>Bird Sanctuary Project</h1>
+      <div className="grid">
+        <div className="side-bar">
+          <Cart cart={cart} setCart={setCart} />
+          <Checkout setCart={setCart} />
+        </div>
+        <div className="cards">
+          {birdData.map((bird) => (
+            <Bird bird={bird} key={bird.id} addToCart={addToCart} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
