7daef7e7abd55cfeb613dcf9958a42d7d8dcaf7d
zazimov <zalmanazimov@pursuit.org>
2023-04-28 11:33:08 -0400

    added Cart info and all tests pass

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -16,5 +16,33 @@ export default function Cart({ adoptBirds, handleRemoveBird }) {
   if (totalprice >= 1000) bonus = bonusItems.slice();
   //   console.log(bonus);
 
-  return <h3>hello</h3>;
+  return (
+    <div className="Cart">
+      <h2>Cart</h2>
+      <h5>Discount: {adoptBirds.length >= 3 ? 10 : 0}%</h5>
+      <h4>Total: ${totalprice}</h4>
+      <ol>
+        {adoptBirds.map((bird, index) => {
+          return (
+            <li key={bird.id}>
+              {bird.name}: ${bird.amount}
+              <button
+                onClick={() => {
+                  handleRemoveBird(index);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ol>
+      <p>Your donation has qualified you for the following items:</p>
+      <ul className="bonuses">
+        {bonus.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
