95f0120d57b19dd0fbfb547fa4a96a8c6bb27e07
lgxxhi <luisgsilva95@gmail.com>
2023-05-02 09:57:32 -0400

    Bird Sanctuary Project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
-function App () {
+import BirdCards from "./BirdCards";
+import birdData from "./data/birds";
+import Cart from "./Cart";
+import Checkout from "./Checkout";
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  let [total, setTotal] = useState(0);
+  const [birdsAdded, setBirdsAdded] = useState([]);
+  const [discount, setDiscount] = useState(0);
+  const [bonus, setBonus] = useState([]);
+
+  function handleCart(e) {
+    setTotal((total += Number(e.target.value)));
+
+    setBirdsAdded((birdsAdded) => [
+      e.target.name + ` $${e.target.value}.00`,
+      ...birdsAdded,
+    ]);
+
+    console.log(birdsAdded);
+  }
+
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="App">
+      <aside>
+        <Cart
+          total={total}
+          birdsAdded={birdsAdded}
+          birdData={birdData}
+          discount={discount}
+          setBirdsAdded={setBirdsAdded}
+          bonus={bonus}
+        />
+      </aside>
+
+      <aside>
+        <Checkout
+          setDiscount={setDiscount}
+          setBirdsAdded={setBirdsAdded}
+          setTotal={setTotal}
+          setBonus={setBonus}
+          birdsAdded={birdsAdded}
+        />
+      </aside>
+
+      <main>
+        <BirdCards
+          birdData={birdData}
+          handleCart={handleCart}
+          setBirdsAdded={setBirdsAdded}
+        />
+      </main>
     </div>
   );
-};
+}
 
 export default App;
