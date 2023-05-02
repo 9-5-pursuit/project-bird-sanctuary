8d81004bbca2eb77c6990910f72e97379ec62ea2
jeremytjimenez <jeremytjimenez@gmail.com>
2023-05-01 20:18:46 -0400

    fixed compile error

--- src/Components/BirdCard.js
+++ src/Components/BirdCard.js
@@ -2,10 +2,10 @@ export default function BirdCard({ bird, adoptBird }) {
 
 
     return (
-    <div className="card" style={{border: "2px solid black", ["maxWidth"]: "300px", ["maxHeight"]: "400px"}}>
+    <div className="card" style={{border: "2px solid black"}}>
         <h3>{bird.name}</h3>
         <p>Price: {bird.amount}</p>
-        <img src={bird.img} alt="bird" style={{["maxWidth"]: "300px"}}/>
+        <img src={bird.img} alt="bird"/>
         <button onClick={() => {adoptBird(bird)}}>ADOPT</button>
     </div>)
 }
